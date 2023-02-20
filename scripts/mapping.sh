186d19e2f46ac958401aae4c803f6f7daa8bed44
Jon Bråte <jonbra@login-3.saga.sigma2.no>
2023-02-20 19:20:39 +0100

    cleaned up

--- scripts/mapping.sh
+++ scripts/mapping.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
-# Usage: bash scripts/mapping.sh data/NC_045512.fa data/reads_file.fastq.gz
+# Usage: bash scripts/mapping.sh data/reads_file.fastq.gz outfile
 cd ~/Module-11-Genome-Assembly
 
 # create results folder
 mkdir -p results/mapping
 
 # Map PacBio HiFi/CCS reads
-./minimap2/minimap2 -ax map-pb $1 $2 > results/mapping/mapping.sam
+./minimap2/minimap2 -ax map-pb data/NC_045512.fa $1 > results/mapping/$2.sam
 
 #sort and index the bam file:
 module load SAMtools/1.15.1-GCC-11.3.0
 
 cd results/mapping
-samtools view -Sb mapping.sam | samtools sort - -o mapping.sorted.bam
-samtools index mapping.sorted.bam
+samtools view -Sb $2.sam | samtools sort - -o ${2%%.sam}.sorted.bam
+samtools index ${2%%.sam}.sorted.bam
 
 #create consensus
 
