f4c55562ddd78d138cb13decd87f0b0aba8ffaf3
Jon Bråte <jonbra@login-5.saga>
2023-02-20 15:58:22 +0100

    added file

---
+++ scripts/consensus.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Usage: bash scripts/mapping.sh data/NC_045512.fa data/reads_file.fastq.gz
+cd ~/Module-11-Genome-Assembly
+
+# create results folder
+mkdir -p results/consensus
+
+# Firs we create a pileup file which calculates the likelihood of each nucleotide at each position given the coverage.
+
+# The call command makes the actual base calls - i.e. sets the nucleotide
+# -m means default calling method. -v output only variants to the reference (to save space). -O sets the output format (uncompressed BCF output, or -z compressed).
+# max-depth sets the maximum number of reads considered per position
+module load BCFtools/1.15.1-GCC-11.3.0 
+bcftools mpileup --max-depth 5000 -Ou -f data/NC_045512.fa results/mapping/mapping.sorted.bam | bcftools call -mv -Oz -o results/consensus/calls.vcf.gz
+bcftools index results/consensus/calls.vcf.gz
+
+cat data/NC_045512.fa | bcftools consensus results/consensus/calls.vcf.gz > results/consensus/consensus.fa
+
