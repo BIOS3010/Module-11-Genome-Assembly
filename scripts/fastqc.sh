072746737b3b8d335fb913c472b2f2f02545c370
Jon Bråte <jon.brate@fhi.no>
2023-04-24 20:04:57 +0200

    deleted file

--- scripts/fastqc.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-module load FastQC/0.11.9-Java-11
-
-fastqc data/SRR19910221.fastq.gz -o results/fastqc
--- scripts/install_minimap2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd ~/Module-11-Genome-Assembly
-git clone https://github.com/lh3/minimap2
-cd minimap2 && make
